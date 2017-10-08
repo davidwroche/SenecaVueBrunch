ac5104bece8ac40a6d3c25ec53e76328fdb68970
davidwroche <davidwroche@yahoo.co.uk>
2017-10-08 07:31:35 +0100

    Fully working SenecaVueBrunch

--- VueBrunch/brunch-config.js
+++ VueBrunch/brunch-config.js
@@ -3,22 +3,20 @@ module.exports = {
     javascripts: {
       joinTo: {
         'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
-        'app.js': /^app/,
+        'app.js': /^app/
       }
     },
-      stylesheets: {
-          joinTo: "css/app.css"
-      },
     templates: {
       joinTo: 'app.js'
-    },
+    }
   },
   plugins: {
     babel: {
       presets: ['latest', 'stage-3']
     },
-    vue: {
-      extractCSS: true
-    }
+      vue: {
+          extractCSS: true,
+          out: './public/styles/app.css'
+      }
   }
 }
